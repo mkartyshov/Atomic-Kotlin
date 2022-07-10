18f77a5730c5bb03e3c5facde483608e350ac443
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-10 13:13:09 +0300

    Create exercise2.kt

---
+++ programming-basics/var&val/exercise2.kt
@@ -0,0 +1,11 @@
+package varAndValExercise2
+
+// defining var a 10, val b equals a, after that assigning 42 to a and display
+
+fun main() {
+  var a = 10
+  val b = a
+  a = 42
+  println(a)
+  println(b)
+}
