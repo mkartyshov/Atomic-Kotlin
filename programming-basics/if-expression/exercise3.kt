99fc627fff88f5f88f102b4340d18897f200a0a2
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-11 14:53:47 +0300

    Create exercise3.kt

---
+++ programming-basics/if-expression/exercise3.kt
@@ -0,0 +1,11 @@
+package ifExpressionsExercise3
+
+fun findMax(first: Int, second: Int): Int =
+  if (first > second)
+    first
+  else
+    second
+
+fun main() {
+  println(findMax(-1, 4))  // 4
+}
