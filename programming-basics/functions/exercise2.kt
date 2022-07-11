f2a5aa07bab61f8c2b2dc8af2eb0903f7cd0ebd8
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-11 13:30:18 +0300

    Create exercise2.kt

---
+++ programming-basics/functions/exercise2.kt
@@ -0,0 +1,9 @@
+package functionsExercise2
+
+// taking three Double and returning sum
+
+fun getSum(a: Double, b: Double, c: Double): Double = a + b + c
+
+fun main() {
+  println(getSum(1.0, 2.2, 3.4))  // 6.6
+}
