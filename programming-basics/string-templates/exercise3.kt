35db4f0093ed73f35b04d309ed515e649439cc1b
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-11 22:01:20 +0300

    Update exercise3.kt

--- programming-basics/string-templates/exercise3.kt
+++ programming-basics/string-templates/exercise3.kt
@@ -1,5 +1,7 @@
 package stringTemplatesExercise3
 
+// writing a function show() with four parameters; the function must display parameters in a specific way
+
 fun show(i: Int, s: String, c: Char, d: Double) {
     println("i: $i")
     println("s: \"$s\"")
