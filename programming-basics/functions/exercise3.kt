208f58b542940795ca8fe8623ed70e9d004c8684
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-11 13:36:32 +0300

    Create exercise3.kt

---
+++ programming-basics/functions/exercise3.kt
@@ -0,0 +1,9 @@
+package functionsExercise3
+
+// creating a function that repeating String twice
+
+fun duplicate(s: String): String = s + s
+
+fun main() {
+  println(duplicate("abc"))  // abcabc
+}
