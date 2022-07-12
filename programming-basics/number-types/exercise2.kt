06707413a3e617bc22d92e3bf9c944017ec48a82
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-12 14:50:54 +0300

    Create exercise2.kt

---
+++ programming-basics/number-types/exercise2.kt
@@ -0,0 +1,15 @@
+package numberTypesExercise2
+
+// checking which values can't be stored in an Int type
+
+val million: Int = 1_000_000
+val millionType: String = "Int"
+
+val billion = 1_000_000_000
+val billionType: String = "Int"
+
+val trillion = 1_000_000_000_000
+val trillionType: String = "Long"
+
+val quintillion = 1_000_000_000_000_000_000
+val quintillionType: String = "Long"
