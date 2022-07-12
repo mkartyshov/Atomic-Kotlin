e853d5bcf049b2d85c8ec8d6ec9625b24b389cf2
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-12 22:10:26 +0300

    Update exercise4.kt

--- programming-basics/the-in-keyword/exercise4.kt
+++ programming-basics/the-in-keyword/exercise4.kt
@@ -6,7 +6,7 @@ package theInKeywordExercise4
 fun isValidCharacter(ch: Char): Boolean = ch in 'a'..'z' || ch in 'A'..'Z' || ch in '0'..'9' || ch == '_'
 
 fun isValidIdentifier(s: String): Boolean {
-  if (s[0] in '0'..'9' || s.isEmpty()) return false
+  if (s.isEmpty() || s[0] in '0'..'9') return false
   for (ch in s) {
     if (!isValidCharacter(ch)) return false
   }
