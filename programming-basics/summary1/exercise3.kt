09a512ddc0155280b1a99905b04cabe6f74ac6e9
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-13 19:26:17 +0300

    Update exercise3.kt

--- programming-basics/summary1/exercise3.kt
+++ programming-basics/summary1/exercise3.kt
@@ -1,10 +1,10 @@
 package summaryIExercise3
 
 fun first(a: Boolean, b: Boolean, c: Boolean): Boolean = // function takes three boolean then And them to produce the result
-  if (a) b && c else false
+  if (a) b && c else false // comparing all three parameters to be true
 
 fun second(a: Boolean, b: Boolean, c: Boolean): Boolean = // same as fun first, but Or the parameters
-  if (a) true else b || c
+  if (a) true else b || c // if a is true, then we display true, but if a is not true, then we have to compare b and c 
 
 fun main() {
   println(first(true, true, true))
@@ -13,3 +13,9 @@ fun main() {
   println(second(false, false, false))
   println(second(false, true, false))
 }
+/* Output:
+true
+false
+false
+true
+*/
