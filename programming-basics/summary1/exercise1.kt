8acbcc0ace6908126e064b458c3e47b17e2d598f
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-13 17:36:15 +0300

    Create exercise1.kt

---
+++ programming-basics/summary1/exercise1.kt
@@ -0,0 +1,14 @@
+package summaryIExercise1
+
+// define var and 2 val, next assign 2 to x, next display the result
+
+fun main() {
+  var x = 1
+  val y = x
+  val z = y
+  x ++
+
+  println(x)
+  println(y)
+  println(z)
+}
