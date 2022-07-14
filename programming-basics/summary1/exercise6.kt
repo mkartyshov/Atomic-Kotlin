c63be05710f2cb154a4dba359bb2d6e63da194a3
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-14 13:59:06 +0300

    Update exercise6.kt

--- programming-basics/summary1/exercise6.kt
+++ programming-basics/summary1/exercise6.kt
@@ -1,9 +1,12 @@
+package summaryIExercise6
+
 fun everyFifthNonSpace(s: String) {
   var x = 1
   for (c in s) {
     if (x % 5 == 0 && c != ' ') { // check if c is every fifth and not blank
       println(c)
     }
+    if (c != ' ') x++ // if c is blank, we go to the next letter
   }
 }
 
