34aca53e8931bd85b1d49ad67608b9d693e652a9
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-18 16:45:29 +0300

    Update exercise1.kt

--- introduction-to-objects/objects-everywhere/exercise1.kt
+++ introduction-to-objects/objects-everywhere/exercise1.kt
@@ -1,1 +1,8 @@
-//
+package objectsEverywhereExercise1
+
+fun isPalindrome(s: String): Boolean = s == s.reversed() // check if String is equal to its reversed version
+
+fun main() {
+  println(isPalindrome("mom"))     // true
+  println(isPalindrome("street"))  // false
+}
