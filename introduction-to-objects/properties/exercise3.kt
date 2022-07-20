4f76dd2e1230eaf6b12e67c031dae3f74a18a6d5
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-20 15:57:27 +0300

    Create exercise3.kt

---
+++ introduction-to-objects/properties/exercise3.kt
@@ -0,0 +1,52 @@
+package propertiesExercise3
+
+class Robot {
+  var x = 0
+  var y = 0
+  val fieldSize = 100 // determine a field size
+
+  fun crossBoundary(location: Int): Int { // creating a function to cross boundaries
+    val inside = location % fieldSize // formula to detect the right position
+    return if (inside < 0) {
+      fieldSize + inside // if the right position is less than 0, then we subtract inside from fieldSize
+    } else {
+      inside // if the right position is bigger than 0, then we just leave the inside
+    }
+  }
+
+  fun right(steps: Int) {
+    x += steps
+    x = crossBoundary(x)
+  }
+
+  fun left(steps: Int) {
+    x -= steps
+    x = crossBoundary(x)
+  }
+
+  fun down(steps: Int) {
+    y += steps
+    y = crossBoundary(y)
+  }
+
+  fun up(steps: Int) {
+    y -= steps
+    y = crossBoundary(y)
+  }
+
+  fun getLocation(): String = "($x, $y)"
+}
+
+fun main() {
+  val robot = Robot()
+  println(robot.getLocation())
+  robot.up(1)
+  println(robot.getLocation())
+  robot.left(10)
+  println(robot.getLocation())
+}
+/* Output:
+(0, 0)
+(0, 99)
+(90, 99)
+*/
