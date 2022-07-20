09441e940634d680e594cf0b35f18923885c46e9
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-20 13:26:47 +0300

    Update exercise1.kt

--- introduction-to-objects/creating-classes/exercise1.kt
+++ introduction-to-objects/creating-classes/exercise1.kt
@@ -1,1 +1,9 @@
-//
+package creatingClassesExercise1
+
+// creating a class named SomeClass with three member functions
+
+class SomeClass {
+    fun a() = println("42") // display 42
+    fun b() = a() // call fun a()
+    fun c() = this.b() // call fun b() by qualifying it
+}
