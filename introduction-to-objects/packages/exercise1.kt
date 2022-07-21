a83173580f9010638d408d01bfc38792aae8e79d
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-21 17:50:56 +0300

    Create exercise1.kt

---
+++ introduction-to-objects/packages/exercise1.kt
@@ -0,0 +1,8 @@
+package packagesExercise1
+import kotlin.math.PI // import two constants from library
+import kotlin.math.E
+
+fun main() { // display value of constants
+  println(PI)
+  println(E)
+}
