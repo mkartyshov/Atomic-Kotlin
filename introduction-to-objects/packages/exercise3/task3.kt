d496155050e90ac5ee5dcac55bff79658760ac57
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-21 18:16:26 +0300

    Create task3.kt

---
+++ introduction-to-objects/packages/exercise3/task3.kt
@@ -0,0 +1,7 @@
+package packagesExercise3
+import pythagorean.EquilateralTriangle // import class from pythagorean package
+
+fun main() {
+    val et = EquilateralTriangle(1.0)
+    println(et.area()) // 0.4330127018922193
+}
