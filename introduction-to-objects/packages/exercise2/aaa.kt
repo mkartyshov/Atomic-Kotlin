997c9aa5343e32266c04f5d12c43b48a75a8f590
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-21 18:06:29 +0300

    Create aaa.kt

---
+++ introduction-to-objects/packages/exercise2/aaa.kt
@@ -0,0 +1,9 @@
+package aaa
+
+val x = 10 // defining a value x
+
+fun fa(i: Int) = i * x // top-level function that takes and Int parameter i and return that argument multiplied by x
+
+class K {
+    override fun toString() = "K" // class K returns "K"
+}
