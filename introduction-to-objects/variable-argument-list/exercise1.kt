9a474985b4e8f7b7e1e9eb6a832cffd8c56bd04e
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-22 15:42:14 +0300

    Create exercise1.kt

---
+++ introduction-to-objects/variable-argument-list/exercise1.kt
@@ -0,0 +1,7 @@
+package variableArgumentListsExercise1
+
+// fun test(vararg x: Char, vararg y: Int) {} We can't use more than one vararg parameter for a function
+
+fun main() {
+  println("Multiple vararg-parameters are prohibited")
+}
