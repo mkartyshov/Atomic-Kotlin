91040fe4790cca733bc8090ee1de613be0d2168b
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-27 14:00:39 +0300

    Update exercise2.kt

--- introduction-to-objects/property-accessors/exercise2.kt
+++ introduction-to-objects/property-accessors/exercise2.kt
@@ -1,32 +1,29 @@
 package propertyAccessorsExercise2
 
-import atomictest.eq
-
 class Accessible {
     var mutable: String = ""
-    get() {
+    get() { // we get the mutable string
         println("mutable:get")
         return field
     }
-    set(value) {
+    set(value) { // we change the mutable string
         println("mutable:set")
         field = value
     }
     val readOnly: String
-        get() {
+        get() { // we get the readonly string
             println("readOnly:get")
-            return mutable
+            return mutable // we return the mutable, but because it is a val now, it will return the get() of mutable
         }
 }
 
 fun main() {
   val accessible = Accessible()
-  accessible.mutable = "abc"
-  accessible.readOnly eq "abc"
+  accessible.mutable
+  accessible.readOnly
 }
 /* output:
-mutable:set
-readOnly:get
-mutable:get
-abc
+mutable:set // we get it after the set(value) of mutable
+readOnly:get // the first line from get() readOnly
+mutable:get // return the mutable
  */
