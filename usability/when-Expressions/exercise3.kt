98c8187100c6dbc33f3c5fe89b47f72381217e69
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-28 16:59:33 +0300

    Update exercise3.kt

--- usability/when-Expressions/exercise3.kt
+++ usability/when-Expressions/exercise3.kt
@@ -4,13 +4,13 @@ import atomictest.eq
 fun balanced(input: String): Boolean {
   var x = 0
   for (c in input) {
-    when (c) {
+    when (c) { // for every ( we add 1, for every ) we subtract 1. In the end we should have zero 
       '(' -> x++
       ')' -> x--
       ' ' -> {}
       else -> throw IllegalArgumentException("The string must consist only of parentheses and whitespace")
     }
-    if (x < 0) return false
+    if (x < 0) return false // if x becomes less than 0, it means that our string is unbalanced; it should start with (, not with )
   }
   return x == 0
 }
