cea58571aaf2c98f61e6de9b3f9202f7957a28bb
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-28 15:54:55 +0300

    Create exercise3.kt

---
+++ usability/overloading/exercise3.kt
@@ -0,0 +1,46 @@
+package overloadingExercise3
+import atomictest.eq
+
+fun List<Int>.joinToString(
+  separator: String = ", ",
+  prefix: String = "",
+  postfix: String = ""
+): String {
+  return joinTo(StringBuilder(), separator, prefix, postfix).toString()
+}
+
+fun List<Int>.myJoinToString(
+  separator: String,
+  prefix: String,
+  postfix: String
+): String {
+  return "MY: " + joinToString(separator, prefix, postfix)
+}
+
+fun List<Int>.myJoinToString(separator: String, prefix: String): String {
+  return myJoinToString(separator, prefix, "")
+}
+
+fun List<Int>.myJoinToString(separator: String): String {
+  return myJoinToString(separator, "", "")
+}
+
+fun List<Int>.myJoinToString(): String {
+  return myJoinToString(", ", "", "")
+}
+
+/* Conflicting overload function
+fun List<Int>.myJoinToString(prefix: String, postfix: String): String {
+  TODO()
+}
+*/
+
+fun main() {
+  val list = listOf(1, 2, 3)
+  list.myJoinToString() eq
+    "MY: 1, 2, 3"
+  list.myJoinToString(separator = "|") eq
+    "MY: 1|2|3"
+  list.myJoinToString(separator = "..", prefix = "Numbers: ") eq
+    "MY: Numbers: 1..2..3"
+}
