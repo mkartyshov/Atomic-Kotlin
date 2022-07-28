aa52dde9d6ca90570234699146aefbe4d73a4415
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-28 14:51:02 +0300

    Update exercise3.kt

--- usability/named&default-arguments/exercise3.kt
+++ usability/named&default-arguments/exercise3.kt
@@ -2,7 +2,7 @@ package namedAndDefaultArgumentsExercise3
 import atomictest.eq
 
 fun joinComments(s: String): String =
-  TODO()
+  s.trimMargin("// ").lines().joinToString("; ") // displaying String in a more fancy way
 
 fun main() {
   val s = """
