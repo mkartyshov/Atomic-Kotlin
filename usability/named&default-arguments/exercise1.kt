43e17b3995814cf868cfff8404df5568d61fba37
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-28 14:35:21 +0300

    Update exercise1.kt

--- usability/named&default-arguments/exercise1.kt
+++ usability/named&default-arguments/exercise1.kt
@@ -1,9 +1,16 @@
-//package namedAndDefaultArgumentsExercise1
+package namedAndDefaultArgumentsExercise1
 
-class Rectangle
+class Rectangle(val side1: Double = 1.0, val side2: Double = 2.0, val color: String = "yellow") {
+  override fun toString() = "$color rectangle $side1 x $side2"
+}
 
 fun main() {
-  println(Rectangle())
-
-  // TODO
+  Rectangle()
+  
+  Rectangle(1.0, 2.0, "yellow") // No names for arguments
+  
+  Rectangle(side1 = 1.0, side2 = 2.0) // With names for arguments
+  Rectangle(color = "yellow")
+  
+  Rectangle(1.0, side2 = 2.0, "yellow") // Mixed
 }
