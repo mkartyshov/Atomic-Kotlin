b140c97588b74edc944e5419019ec78fab94a80e
Mikhail Kartyshov <mkartyshov@proton.me>
2022-07-30 16:44:59 +0300

    Update exercise2.kt

--- usability/enumirations/exercise2.kt
+++ usability/enumirations/exercise2.kt
@@ -5,7 +5,7 @@ import enumerationsExercise2.Season.*
 
 enum class Month {
   January, February, March, April,
-  // May, June, July, August,
+  May, June, July, August,
   September, October, November, December
 }
 
@@ -13,15 +13,24 @@ enum class Season {
   Winter, Spring, Summer, Autumn
 }
 
-fun Month.getAustralianSeason(): Season = TODO()
+fun Month.getAustralianSeason(): Season = when (this) { // different seasons in australia
+  December, January, February -> Summer // when we call for month, we return the season
+  March, April, May -> Autumn
+  June, July, August -> Winter
+  September, October, November -> Spring
+}
 
-fun Month.getDays(leapYear: Boolean = false): Int = TODO()
+fun Month.getDays(leapYear: Boolean = false): Int = when (this) {
+  January, March, May, July, August, October, December -> 31 // when we call for month, we return amount of days
+  April, June, September, November -> 30
+  February -> if (leapYear) 29 else 28 // 29 or 28 fays for february
+}
 
 fun main() {
- // January.getAustralianSeason() eq Summer
-  //May.getAustralianSeason() eq Autumn
+  January.getAustralianSeason() eq Summer
+  May.getAustralianSeason() eq Autumn
 
- // January.getDays() eq 31
-//  February.getDays(leapYear = false) eq 28
- // February.getDays(leapYear = true) eq 29
+  January.getDays() eq 31
+  February.getDays(leapYear = false) eq 28
+  February.getDays(leapYear = true) eq 29
 }
