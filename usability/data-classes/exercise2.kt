b7e736f1570607664954198a04ae87608f497a5c
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-02 21:27:16 +0300

    Update exercise2.kt

--- usability/data-classes/exercise2.kt
+++ usability/data-classes/exercise2.kt
@@ -1,36 +1,25 @@
-package enumerationsExercise2
-import atomictest.eq
-import enumerationsExercise2.Month.*
-import enumerationsExercise2.Season.*
+package dataClassesExercise2
 
-enum class Month {
-  January, February, March, April,
-  May, June, July, August,
-  September, October, November, December
-}
+data class AirlineTicket(
+  val firstName: String,
+  val lastName: String,
+  val ticket: Int,
+  val origin: String,
+  val destination: String
+)
 
-enum class Season {
-  Winter, Spring, Summer, Autumn
-}
-
-fun Month.getAustralianSeason(): Season = when (this) { // different seasons in australia
-  December, January, February -> Summer // when we call for month, we return the season
-  March, April, May -> Autumn
-  June, July, August -> Winter
-  September, October, November -> Spring
-}
-
-fun Month.getDays(leapYear: Boolean = false): Int = when (this) {
-  January, March, May, July, August, October, December -> 31 // when we call for month, we return amount of days
-  April, June, September, November -> 30
-  February -> if (leapYear) 29 else 28 // 29 or 28 fays for february
+fun AirlineTicket.transferTicket(
+  otherFirstName: String,
+  otherLastName: String
+): AirlineTicket {
+  return copy(firstName = otherFirstName, lastName = otherLastName)
 }
 
 fun main() {
-  January.getAustralianSeason() eq Summer
-  May.getAustralianSeason() eq Autumn
-
-  January.getDays() eq 31
-  February.getDays(leapYear = false) eq 28
-  February.getDays(leapYear = true) eq 29
+  val ticket = AirlineTicket("Bruce", "Eckel",
+    123456, "DEN", "HND")
+  println(ticket.transferTicket("Svetlana", "Isakova"))
 }
+/* Output:
+AirlineTicket(firstName=Svetlana, lastName=Isakova, ticket=123456, origin=DEN, destination=HND)
+*/
