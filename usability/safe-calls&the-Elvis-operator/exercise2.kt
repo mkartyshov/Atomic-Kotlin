f88140142b9cadad7885e524e161bba7c8a64fc1
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-05 13:39:45 +0300

    Create exercise2.kt

---
+++ usability/safe-calls&the-Elvis-operator/exercise2.kt
@@ -0,0 +1,24 @@
+package safeCallsAndTheElvisOperatorExercise2
+import atomictest.eq
+
+data class Client(
+  val name: String,
+  val personalInfo: PersonalInfo? = null)
+
+data class PersonalInfo(
+  val email: String? = null,
+  val address: Address? = null)
+
+data class Address(
+  val country: String? = null,
+  val city: String? = null,
+  val street: String? = null)
+
+fun Client.fromMunich(): Boolean = personalInfo?.address?.city == "Munich" // check if client from Munich
+
+fun main() {
+  val alice = Client("Alice",
+    PersonalInfo(address = Address("Germany", "Munich")))
+  alice.fromMunich() eq true
+  Client("Bob").fromMunich() eq false
+}
