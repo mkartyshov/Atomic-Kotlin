580af46287faf590e5102c9be7e1d19cd569abab
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-06 23:19:08 +0300

    Update exercise2.kt

--- usability/extension-properties/exercise2.kt
+++ usability/extension-properties/exercise2.kt
@@ -1,11 +1,10 @@
 package extensionPropertiesExercise2
 import atomictest.eq
 
-// TODO: implement 'reversed'
+val <T> List<T>.reversed: List<T> // function to reverse list as an extension property
+    get() = reversed()
 
 fun main() {
-/*
   val list = listOf(1, 2, 3)
   list.reversed eq listOf(3, 2, 1)
-*/
 }
