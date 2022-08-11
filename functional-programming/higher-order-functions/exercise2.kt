95d1eb5d91b86fb327de95ec2f9e649950be2ea5
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-11 16:26:39 +0300

    Update exercise2.kt

--- functional-programming/higher-order-functions/exercise2.kt
+++ functional-programming/higher-order-functions/exercise2.kt
@@ -2,10 +2,10 @@ package higherOrderFunctionsExercise2
 import atomictest.eq
 
 fun List<Int>.transformVersion1(): List<Int> =
-  filter { it % 2 == 0 }.map { it * it }
+  filter { it % 2 == 0 }.map { it * it } // we use filter to find element from list; multiply it by itself
 
 fun List<Int>.transformVersion2(): List<Int> =
-  mapNotNull { if (it % 2 == 0) it * it else null } 
+  mapNotNull { if (it % 2 == 0) it * it else null } // we use if else to get the same result
 
 fun main() {
   val list = listOf(1, 2, 3)
