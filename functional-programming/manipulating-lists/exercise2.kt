61edda49236b8cc549e4fba7bb73b8a9a5441bfc
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-11 20:46:04 +0300

    Create exercise2.kt

---
+++ functional-programming/manipulating-lists/exercise2.kt
@@ -0,0 +1,11 @@
+package manipulatingListsExercise2
+import atomictest.eq
+
+fun <T> List<T>.zipWithIndex(): List<Pair<T, Int>> =
+  mapIndexed { index, e -> Pair(e, index) } // convert list to list of elements with index
+
+fun main() {
+  val list = listOf('a', 'b', 'c')
+  list.zipWithIndex() eq
+    "[(a, 0), (b, 1), (c, 2)]"
+}
