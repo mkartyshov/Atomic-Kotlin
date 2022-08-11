e8275eace7697c66f079110a26814f37f442f347
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-11 16:34:52 +0300

    Update exercise4.kt

--- functional-programming/higher-order-functions/exercise4.kt
+++ functional-programming/higher-order-functions/exercise4.kt
@@ -7,7 +7,7 @@ fun main() {
   val multiply2: (Int) -> Int = { x: Int -> x * 2 }
   val println: (Int) -> Unit = { i: Int -> println("Result: $i") }
 
-  val composition = multiply2.andThen(println)
+  val composition = multiply2.andThen(println) // we first multiply Int by 2 and after that return the result as a printed String
   composition(5)
 }
 /* Output:
