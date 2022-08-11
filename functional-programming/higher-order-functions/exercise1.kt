b4003e8e4337f3f4dbd71ed767b617c9163118d9
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-11 16:23:25 +0300

    Update exercise1.kt

--- functional-programming/higher-order-functions/exercise1.kt
+++ functional-programming/higher-order-functions/exercise1.kt
@@ -1,12 +1,12 @@
 package higherOrderFunctionsExercise1
 import atomictest.eq
 
-fun <T, R> List<T>.map(transform: (T) -> R): List<R> {
-  val result = mutableListOf<R>()
-  for (e in this) {
-    result += transform(e)
+fun <T, R> List<T>.map(transform: (T) -> R): List<R> { // function returns a transformed list
+  val result = mutableListOf<R>() // final result is transformed list
+  for (e in this) { // for elements in original list
+    result += transform(e) // add transformed elements to the result list
   }
-  return result
+  return result // return the final result
 }
 
 fun main() {
