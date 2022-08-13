a7df189d6f53dd4259cf4c3fd6bca2486d2388ef
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-13 23:13:19 +0300

    Update exercise1.kt

--- functional-programming/sequences/exercise1.kt
+++ functional-programming/sequences/exercise1.kt
@@ -1,9 +1,9 @@
 package sequencesExercise1
 
-fun School.commonLessons(
+fun School.commonLessons( //  function to find all the lessons that a student is taking with a particular instructor
   instructor: Instructor,
   student: Student
 ): Sequence<Lesson> {
-  return lessons.filter { //  find all the lessons that a student is taking with a particular instructor.
+  return lessons.filter { 
     instructor == it.instructor && student in it.students // instructor is in lesson && student is in lesson
   }
