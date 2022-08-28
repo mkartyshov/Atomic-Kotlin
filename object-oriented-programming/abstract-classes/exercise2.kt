c3ca5e5a69d6388d2e91dd94eb9ff0bd5f10bd79
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-28 23:54:46 +0300

    Update exercise2.kt

--- object-oriented-programming/abstract-classes/exercise2.kt
+++ object-oriented-programming/abstract-classes/exercise2.kt
@@ -9,12 +9,10 @@ abstract class Sleepable {
   fun sleepOn() = "sleep"
 }
 
-// class Sofa: Movable(), Sleepable()
+class Sofa: Movable(), Sleepable()
 
 fun main() {
-/*
   val sofa = Sofa()
   sofa.move() eq "move"
   sofa.sleepOn() eq "sleep"
-*/
 }
