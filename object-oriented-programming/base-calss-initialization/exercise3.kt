5ea15deac9ede9ff8e88f625dbe4dd30585b7913
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-29 17:31:53 +0300

    Update exercise3.kt

--- object-oriented-programming/base-calss-initialization/exercise3.kt
+++ object-oriented-programming/base-calss-initialization/exercise3.kt
@@ -1,16 +1,18 @@
 package baseClassInitializationExercise3
 import atomictest.trace
 
-/* TODO: Implement Animal, Cat and Dog classes */
+open class Animal(val sound: String = "???") // open class Animal
+
+class Cat(sound: String = "meow") : Animal(sound) // inherited class Cat that passes a sound value to the Animal class
+
+class Dog(sound: String = "woof") : Animal(sound) // inherited class Dog that passes a sound value to the Animal class
 
 fun main() {
-/*
   val animal = Animal()
   trace(animal.sound)
   val cat = Cat()
   trace(cat.sound)
   val dog = Dog()
   trace(dog.sound)
-*/
   trace eq "??? meow woof"
 }
