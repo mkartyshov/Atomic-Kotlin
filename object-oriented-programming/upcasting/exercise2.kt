0c1c1205ec569ae6cbe58bf1af83a4461f1bb4b8
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-29 05:43:46 +0300

    Create exercise2.kt

---
+++ object-oriented-programming/upcasting/exercise2.kt
@@ -0,0 +1,26 @@
+package upcastingExercise2
+import atomictest.eq
+
+interface Apple {
+  fun consume(): String
+}
+
+class GrannySmith
+
+class Gala
+
+class Fuji
+
+class Braeburn
+
+fun main() {
+  val apples = listOf(
+    GrannySmith(),
+    Gala(),
+    Fuji(),
+    Braeburn()
+  )
+  apples.map { TODO() } eq
+    "[chomp GrannySmith, " +
+    "bite Gala, press Fuji, peel Braeburn]"
+}
