6593236e880bb4ab7c87db0945ab33ac0694c29c
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-29 17:23:09 +0300

    Update exercise1.kt

--- object-oriented-programming/base-calss-initialization/exercise1.kt
+++ object-oriented-programming/base-calss-initialization/exercise1.kt
@@ -1,7 +1,7 @@
 package baseClassInitializationExercise1
 
-open class A(val s: String)
+open class A(val s: String) // open class A
 
-open class B(s: String) : A(s)
+open class B(s: String) : A(s) // open class B inherited from class A
 
-class C(s: String) : B(s)
+class C(s: String) : B(s) // final class C inherited from class B
