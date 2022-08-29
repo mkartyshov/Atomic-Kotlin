ad43d18eddc755fcea31930e813be69399dae956
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-29 17:52:20 +0300

    Update exercise1.kt

--- object-oriented-programming/abstract-classes/exercise1.kt
+++ object-oriented-programming/abstract-classes/exercise1.kt
@@ -1,25 +1,36 @@
 package abstractClassesExercise1
+
 import atomictest.trace
 
-abstract class Shape
+abstract class Shape { // abstract class Shape with to abstract methods, return type Unit
+    abstract fun draw()
+    abstract fun erase() 
+}
+// override abstract methods to call trace():
+class Circle : Shape() { 
+    override fun draw() = trace("Draw Circle") 
+    override fun erase() = trace("Erase Circle")
+}
 
-class Circle
+class Square : Shape() {
+    override fun draw() = trace("Draw Square")
+    override fun erase() = trace("Erase Square")
+}
 
-class Square
-
-class Triangle
+class Triangle : Shape() {
+    override fun draw() = trace("Draw Triangle")
+    override fun erase() = trace("Erase Triangle")
+}
 
 fun main() {
-  val shapes = listOf(
-    Circle(),
-    Square(),
-    Triangle(),
-  )
-
-  shapes.forEach { it.draw() }
-  shapes.forEach { it.erase() }
-
-  trace eq """
+    val shapes = listOf(
+        Circle(),
+        Square(),
+        Triangle(),
+    )
+    shapes.forEach { it.draw() } // draw() for each object
+    shapes.forEach { it.erase() } // erase() for each object
+    trace eq """
     Draw Circle
     Draw Square
     Draw Triangle
