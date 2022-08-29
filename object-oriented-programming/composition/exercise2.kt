9d9b34d49ebb8ceed7a84da611041f9e4be1a542
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-29 20:33:31 +0300

    Create exercise2.kt

---
+++ object-oriented-programming/composition/exercise2.kt
@@ -0,0 +1,32 @@
+package compositionExercise2
+import atomictest.*
+// modify the starter code to use composition instead of inheritance
+class Stack<E> {
+  private val list = mutableListOf<E>()
+  fun push(e: E) = list.add(e)
+  fun pop(): E = list.removeLast()
+}
+
+class Queue<E> {
+  private val list = mutableListOf<E>()
+  fun put(e: E) = list.add(e)
+  fun poll(): E = list.removeFirst()
+}
+
+fun main() {
+  val stack = Stack<Int>()
+  stack.push(1)
+  stack.push(2)
+  stack.push(3)
+  stack.push(4)
+  stack.pop() eq 4
+  stack.pop() eq 3
+
+  val queue = Queue<String>()
+  queue.put("a")
+  queue.put("b")
+  queue.put("c")
+  queue.put("d")
+  queue.poll() eq "a"
+  queue.poll() eq "b"
+}
