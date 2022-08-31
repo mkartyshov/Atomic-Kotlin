e007e4a3dd4896c1ab18c37c6e937dfe7cb53663
Mikhail Kartyshov <mkartyshov@proton.me>
2022-08-31 23:43:24 +0300

    Create exercise2.kt

---
+++ object-oriented-programming/objects/exercise2.kt
@@ -0,0 +1,31 @@
+package objectsExercise2
+import atomictest.eq
+
+object Outer {
+  object Nested {
+    val a = "Outer.Nested.a"
+    object Nested { // Added more nesting
+      val a = "Outer.Nested.Nested.a"
+    }
+  }
+}
+
+class HasObject {
+  object Nested {
+    val a = "HasObject.Nested.a"
+    object Nested { // Added more nesting
+      val a = "HasObject.Nested.Nested.a"
+    }
+  }
+}
+
+fun main() {
+  Outer.Nested.a eq
+    "Outer.Nested.a"
+  HasObject.Nested.a eq
+    "HasObject.Nested.a"
+  Outer.Nested.Nested.a eq
+    "Outer.Nested.Nested.a"
+  HasObject.Nested.Nested.a eq
+    "HasObject.Nested.Nested.a"
+}
